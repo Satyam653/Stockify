5079e9c1dd4726ffa2f8842bfbd3bd173ca256f6
unknown <satyamk653@gmail.com>
2025-08-23 02:11:08 +0530

    funds in dashboard

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -67,7 +67,7 @@ const Menu = () => {
           <li>
             <Link
               style={{ textDecoration: "none" }}
-              to="funds"
+              to="/funds"
               onClick={() => handleMenuClick(4)}
             >
               <p className={selectedMenu === 4 ? activeMenuClass : menuClass}>
