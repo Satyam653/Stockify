642365c11d753fde98d292d243944c7cda366b09
unknown <satyamk653@gmail.com>
2025-08-23 01:03:21 +0530

    error in axios

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -9,7 +9,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get(" https://stockify-goml.onrender.co").then((res) => {
+    axios.get(" https://stockify-goml.onrender.co/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
