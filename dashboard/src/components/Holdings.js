b5daca2c9b90d8dc46c6833b1b9e4241bbc8bbe4
unknown <satyamk653@gmail.com>
2025-08-23 00:19:31 +0530

    changes in dashboard

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -9,7 +9,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get(" https://stockify-goml.onrender.co").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
